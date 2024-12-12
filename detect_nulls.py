9a91739526318fb01d1aa4c5630f7270f71a36e3
Efe Özgen <efe.ozgennn@gmail.com>
2024-12-12 16:01:22 +0300

    handled duplicate track_id's & labeled features

--- detect_nulls.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def detect_nulls(df):
-    return pd.DataFrame({
-        "nulls": df.isnull().sum(),
-        "Empty Strings": df.applymap(lambda x: x == None).sum()
-    })
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import pandas as pd
-from detect_nulls import detect_nulls
 
 
 def read_raw_data(row_data_path):
     return pd.read_json(row_data_path)
+
+
+def detect_nulls(df):
+    return pd.DataFrame(
+        {
+            "nulls": df.isnull().sum(),
+            "Empty Strings": df.map(lambda x: x == None).sum(),
+        }
+    )
 
 
 def handle_float(df, columns):
@@ -21,6 +29,9 @@ def handle_float(df, columns):
 
 def drop_columns(df, columns):
     return df.drop(columns=columns)
+
+def handle_duplicates(df):
+    return df.drop_duplicates(subset="track_id", keep="first")
 
 
 if __name__ == "__main__":
@@ -49,19 +60,44 @@ if __name__ == "__main__":
     # print(df[columns_to_convert])
 
     columns_to_drop = [
-        "track_id",
         "track_name",
         "track_album_id",
         "track_album_name",
         "playlist_name",
         "playlist_id",
     ]
-    df_clean = drop_columns(df, columns_to_drop)
-    print(df_clean.head())
+    df = drop_columns(df, columns_to_drop)
 
-    # Popularity classification
-    bins = [0, 40, 70, 100]
-    labels = ["Low", "Medium", "High"]
-    df["popularity_class"] = pd.cut(df["track_popularity"].astype(int), bins=bins, labels=labels)
-    
-    print(df["popularity_class"].value_counts())
+    # Popularity labeling
+    bins_popularity = [0, 40, 70, 100]
+    labels_popularity = ["Low", "Medium", "High"]
+    df["popularity_label"] = pd.cut(
+        df["track_popularity"].astype(int),
+        bins=bins_popularity,
+        labels=labels_popularity,
+    )
+
+    # Release date
+    bins_date = [
+        df["track_album_release_date"].min(),
+        "1970-01-01",
+        "1980-01-01",
+        "1990-01-01",
+        "2000-01-01",
+        "2010-01-01",
+        "2015-01-01",
+        "2020-01-01",
+        df["track_album_release_date"].max(),
+    ]
+    labels_popularity = ["<1970", "1970-1980", "1980-1990", "1990-2000", "2000-2010", "2010-2015", "2015-2020", ">2020"]
+    df["release_date_label"] = pd.cut(
+        df["track_album_release_date"].astype(str),
+        bins=bins_date,
+        labels=labels_popularity,
+    )
+
+    # print(df["popularity_class"].value_counts())
+    print("number of duplicate track_id's before handle duplicate =>", df["track_id"].duplicated().sum())
+    df = handle_duplicates(df)
+    print("number of duplicate track_id's after handle duplicate =>", df["track_id"].duplicated().sum())
+    print(df["track_id"].duplicated() == True)
