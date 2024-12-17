812d547dca3885373ff16e6a514a37d81f5ddbab
Efe Özgen <efe.ozgennn@gmail.com>
2024-12-17 19:18:03 +0300

    until drop columns

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import pandas as pd
+import csv
+import json
 
 
-def read_raw_data(row_data_path):
-    return pd.read_json(row_data_path)
+def read_data(datapath):
+    return pd.read_csv(datapath, encoding="utf-8")
 
+def write_clean_data(df):
+    return df.to_csv("data/clean_data.csv")
+    
+def csv_to_json(csv_file, json_file):
+    # CSV dosyasını JSON'a dönüştür
+    data = []
+    with open(csv_file, encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+
+    # JSON dosyasına yaz
+    with open(json_file, 'w', encoding='utf-8') as jsonfile:
+        json.dump(data, jsonfile, ensure_ascii=False, indent=4)
+
+    print(f"CSV dosyasından JSON formatına dönüştürüldü ve {json_file} dosyasına kaydedildi.")
 
 def detect_nulls(df):
     return pd.DataFrame(
@@ -35,8 +53,8 @@ def handle_duplicates(df):
 
 
 if __name__ == "__main__":
-    row_data_path = "data/raw_data.json"
-    df_raw = read_raw_data(row_data_path)
+    datapath = "data/spotify_songs.csv"
+    df_raw = read_data(datapath)
 
     nulls = detect_nulls(df_raw)
 
@@ -58,7 +76,7 @@ if __name__ == "__main__":
 
     df = handle_float(df_raw, columns_to_convert)
     # print(df[columns_to_convert])
-
+    # print(df)
     columns_to_drop = [
         "track_name",
         "track_album_id",
@@ -77,7 +95,7 @@ if __name__ == "__main__":
         labels=labels_popularity,
     )
 
-    # Release date
+    # Release date labeling
     bins_date = [
         df["track_album_release_date"].min(),
         "1970-01-01",
@@ -89,15 +107,20 @@ if __name__ == "__main__":
         "2020-01-01",
         df["track_album_release_date"].max(),
     ]
-    labels_popularity = ["<1970", "1970-1980", "1980-1990", "1990-2000", "2000-2010", "2010-2015", "2015-2020", ">2020"]
+    labels_release_date = ["<1970", "1970-1980", "1980-1990", "1990-2000", "2000-2010", "2010-2015", "2015-2020", ">2020"]
     df["release_date_label"] = pd.cut(
         df["track_album_release_date"].astype(str),
         bins=bins_date,
-        labels=labels_popularity,
+        labels=labels_release_date,
     )
 
     # print(df["popularity_class"].value_counts())
     print("number of duplicate track_id's before handle duplicate =>", df["track_id"].duplicated().sum())
     df = handle_duplicates(df)
     print("number of duplicate track_id's after handle duplicate =>", df["track_id"].duplicated().sum())
-    print(df["track_id"].duplicated() == True)
+    print(df["track_id"].duplicated())
+    
+    df = drop_columns(df, ["track_id"])
+    
+    write_clean_data(df)
+    csv_to_json("data/clean_data.csv", "data/clean_data.json")
